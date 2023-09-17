8e5a5e591055f3b6b6f06683aecac5c009e71b52
devopati <davidopati70@gmail.com>
2023-09-17 09:14:53 +0300

    n

--- frontend/src/Data/marketData.js
+++ frontend/src/Data/marketData.js
@@ -6,19 +6,20 @@ export const marketData = [
     time: "1200",
     price: 18,
     image:
-      "https://cleanaway2stor.blob.core.windows.net/cleanaway2-blob-container/2019/06/Green-organics.jpg",
+      "https://images.unsplash.com/photo-1606441171708-6534789d2d95?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
     likecount: 148,
-    title: "Green Garbagge",
+    title: "Green Garbbage",
     description:
-      "Did you know green garbage reduces disposal costs while also creating a valuable usable product",
-  
+      "Did you know carpet can be recycled?Used carpet is among the top ten materials in California landfills, with more than 350 million pounds discarded each year. However, petroleum-based carpet can be recycled into useful products, including new carpet, carpet padding,...",
+  },
+  {
     id: "kjeg6f5875cklm",
     name: "Mish Ochieng'",
     date: "23 june 2023",
     time: "0700",
     price: 70,
     image:
-      "https://i0.wp.com/mobileskips.com.au/wp-content/uploads/2020/12/Green-waste-sydney.jpg",
+      "https://images.unsplash.com/photo-1528498033373-3c6c08e93d79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGdyZWVuJTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
     likecount: 62,
     title: "Food Waste Recycle",
     description:
@@ -31,7 +32,7 @@ export const marketData = [
     time: "1000",
     price: 55,
     image:
-      "https://www.foodnavigator.com/var/wrbm_gb_food_pharma/storage/images/7/4/4/7/1617447-1-eng-GB/Phytonext-hope-for-cheaper-essential-oil-extracts-from-citrus-fruit.png",
+      "https://images.unsplash.com/photo-1519123439617-f3693cc27f4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
     likecount: 11,
 
     title: "Vegatable waste",
@@ -45,7 +46,7 @@ export const marketData = [
     time: "0926",
     price: 37,
     image:
-      "https://c8.alamy.com/comp/2E27P9B/rotten-old-fruit-and-vegetables-fruit-and-vegetable-waste-on-compost-2E27P9B.jpg",
+      "https://images.unsplash.com/photo-1511993226957-cd166aba52d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI0fHxncmVlbiUyMGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
     likecount: 88,
 
     title: "Vegatable Farm waste",
@@ -59,7 +60,7 @@ export const marketData = [
     time: "0600",
     price: 107,
     image:
-      "https://img.freepik.com/premium-photo/waste-vegetables-fruits-compost-heap-as-fertilizer-garden_317169-1266.jpg?w=2000",
+      "https://images.unsplash.com/photo-1511690743698-d9d85f2fbf38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
     likecount: 11,
 
     title: "Green Rich Food Waste",
