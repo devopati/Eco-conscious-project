924b7decea15d5d6d763345ebe8d604927024666
devopati <davidopati70@gmail.com>
2023-10-03 21:21:20 +0300

    Blogs retrieval and displaying completed

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Features/Auth/authSlice";
+import BlogSlice from "./Features/BlogSlice";
 export const Store = configureStore({
   reducer: {
     auth: authReducer,
+    blog: BlogSlice,
   },
 });
